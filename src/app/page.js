82d57d773744edb7807350e8123c7c587736a14b
gaurezzz <gaurez@outlook.com>
2024-07-15 07:56:21 -0600

    ejemplo2 finalizado

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,73 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
+const Equipos = ({ equipos }) => {
+  return (
+    <div className={styles.container__list}>
+      <h2 className={styles.title}>Equipos de Fútbol</h2>
+      {equipos.map((equipo) => (
+        <div key={equipo.id}>
+          <h3 className={styles.nameclub}>{equipo.nombre}</h3>
+          <ul>
+            {equipo.plantilla.map((jugador) => (
+              <li className={styles.container__list} key={jugador.id}>
+                <strong>{jugador.nombre}</strong>
+                <p>Altura: {jugador.Altura}m <br /> Peso: {jugador.Peso}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default function Home() {
+  // Simula la obtención de datos desde tu JSON
+  const equiposData = [
+    {
+      "id": 1,
+      "nombre": "Real Madrid",
+      "plantilla": [
+        {"id": 1, "nombre": "Eden Hazard", "Altura": "1.75", "Peso": "74Kg"},
+        {"id": 2, "nombre": "Gonzalo García", "Altura": "1.82", "Peso": "74Kg"},
+        {"id": 3, "nombre": "Karim Benzema", "Altura": "1.85", "Peso": "81Kg"}
+      ]
+    },
+    {
+      "id": 2,
+      "nombre": "Barcelona",
+      "plantilla": [
+        {"id": 1, "nombre": "Marc-André ter Stegen", "Altura": "1.75", "Peso": "74Kg"},
+        {"id": 2, "nombre": "IñigoMartinez", "Altura": "1.82", "Peso": "74Kg"},
+        {"id": 3, "nombre": "Gavi", "Altura": "1.85", "Peso": "81Kg"}
+      ]
+    },
+    {
+      "id": 3,
+      "nombre": "Atlético de Madrid",
+      "plantilla": [
+        {"id": 1, "nombre": "Jan Oblak", "Altura": "1.75", "Peso": "74Kg"},
+        {"id": 2, "nombre": "Stefan Savic", "Altura": "1.82", "Peso": "74Kg"},
+        {"id": 3, "nombre": "Koke", "Altura": "1.85", "Peso": "81Kg"},
+        {"id": 4, "nombre": "Luis Suárez", "Altura": "1.85", "Peso": "81Kg"}
+      ]
+    },
+    {
+      "id": 4,
+      "nombre": "Sevilla",
+      "plantilla": [
+        {"id": 1, "nombre": "Yassine Bounou", "Altura": "1.75", "Peso": "74Kg"},
+        {"id": 2, "nombre": "Diego Carlos", "Altura": "1.82", "Peso": "74Kg"},
+        {"id": 3, "nombre": "Joan Jordán", "Altura": "1.85", "Peso": "81Kg"}
+      ]
+    }
+  ];
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <h1>Mi Aplicación de Fútbol</h1>
+        <Equipos equipos={equiposData} />
       </div>
     </main>
   );
